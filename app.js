57835768651db4821b9e2dfad22840568b4c61bd
hyip8000 <hyip@purdue.edu>
2017-06-08 14:54:34 -0400

    Fixed up and down functions and array storage

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = {
       .querySelector(selectors.formSelector)
       .addEventListener('submit', this.addFlickViaForm.bind(this))
     this.load()
+  
   },
 
   // load saved data when browser refreshes
@@ -52,7 +53,9 @@ const app = {
     deleteButton.addEventListener("click", anotherFunction.bind(this))
     promoteButton.addEventListener("click", myFunction)
     
+    
     function upFunction(e){
+      
       const text = e.target.parentNode.textContent[0]
       var index = 0;
       for (var i=0; i<this.flicks.length; i++){
@@ -61,7 +64,6 @@ const app = {
           break
         }
       }
-      this.save()
       
       if (index === 0){
         const curr = this.flicks[index]
@@ -74,8 +76,18 @@ const app = {
         this.flicks[index] = next
       }
       
+      const nextLi = e.target.parentNode.previousElementSibling;
+
+      if(!nextLi){
+        return;
+      }
+
+      if (nextLi.classList.contains('template')){
+        return;
+      }
       this.list.insertBefore(e.target.parentNode, e.target.parentNode.previousSibling)
-        
+      
+      this.save() 
     }
 
     function downFunction(e){
@@ -87,20 +99,32 @@ const app = {
           break
         }
       }
-      this.save()
+      
+      let next = null
+      let curr = null
 
       if (index !== this.flicks.length-1){
-        const next = this.flicks[index+1]
-        const curr = this.flicks[index]
+        next = this.flicks[index+1]
+        curr = this.flicks[index]
         this.flicks[index+1] = curr
         this.flicks[index] = next
+        
       }else{
         return;
       }
+      const nextLi = e.target.parentNode.nextElementSibling;
 
+      if(!nextLi){
+        return;
+      }
+
+      if (nextLi.classList.contains('template')){
+        return;
+      }
       if (e.target.parentNode.nextSibling){
         this.list.insertBefore(e.target.parentNode, e.target.parentNode.nextSibling.nextSibling)
       }
+      this.save()
   }
    
     //delete
@@ -135,9 +159,8 @@ const app = {
         }
     }
 
-
-    this.list
-      .insertBefore(listItem, this.list.firstChild)
+     this.list
+       .insertBefore(listItem, this.list.firstChild)
     
     ++ this.max
     this.flicks.unshift(flick)
@@ -152,124 +175,15 @@ const app = {
       name: f.flickName.value,
     }
 
-  //   const listItem = this.renderListItem(flick)
-  //   var promoteButton = document.createElement('button')
-  //   var deleteButton = document.createElement('button')
-  //   var upButton = document.createElement('button')
-  //   var downButton = document.createElement('button')
-
-  //   upButton.classList.add('up')
-  //   downButton.classList.add('down')
-  //   deleteButton.classList.add('del')
-  //   promoteButton.classList.add('promo')
-  //   promoteButton.classList.add('notclicked')
-  //   promoteButton.textContent = '❤'
-  //   deleteButton.textContent = '🗑'
-  //   upButton.textContent = '👆'
-  //   downButton.textContent = '👇'
-
-  //   listItem.appendChild(promoteButton)
-  //   listItem.appendChild(deleteButton)
-  //   listItem.appendChild(upButton)
-  //   listItem.appendChild(downButton)
-  //   this.list.appendChild(listItem)
-  
-  //   upButton.addEventListener("click", upFunction.bind(this))
-  //   downButton.addEventListener("click", downFunction.bind(this))
-  //   deleteButton.addEventListener("click", anotherFunction.bind(this))
-  //   promoteButton.addEventListener("click", myFunction)
-    
-  //   function upFunction(e){
-  //     const text = e.target.parentNode.textContent[0]
-  //     var index = 0;
-  //     for (var i=0; i<this.flicks.length; i++){
-  //       if (this.flicks[i].name === text) {
-  //         index = i
-  //         break
-  //       }
-  //     }
-  //     this.save()
-      
-  //     if (index === 0){
-  //       const curr = this.flicks[index]
-  //       this.flicks.splice(0,1)
-  //       this.flicks.push(curr)
-  //     }else{
-  //       const next = this.flicks[index-1]
-  //       const curr = this.flicks[index]
-  //       this.flicks[index-1] = curr
-  //       this.flicks[index] = next
-  //     }
-      
-  //     this.list.insertBefore(e.target.parentNode, e.target.parentNode.previousSibling)
-        
-  //   }
-
-  //   function downFunction(e){
-  //     const text = e.target.parentNode.textContent[0]
-  //     var index = 0;
-  //     for (var i=0; i<this.flicks.length; i++){
-  //       if (this.flicks[i].name === text) {
-  //         index = i
-  //         break
-  //       }
-  //     }
-  //     this.save()
-
-  //     if (index !== this.flicks.length-1){
-  //       const next = this.flicks[index+1]
-  //       const curr = this.flicks[index]
-  //       this.flicks[index+1] = curr
-  //       this.flicks[index] = next
-  //     }else{
-  //       return;
-  //     }
-
-  //     if (e.target.parentNode.nextSibling){
-  //       this.list.insertBefore(e.target.parentNode, e.target.parentNode.nextSibling.nextSibling)
-  //     }
-  // }
-   
-  //   //delete
-  //   function anotherFunction(ev){
-  //     for (let i=0; i < this.flicks.length; i++){ //don't use var
-  //       if (this.flicks[i].name === flick.name) {
-  //         this.flicks.splice(i,1);
-  //         break;
-  //       }
-  //     }
-  //     this.list.removeChild(listItem)
-  //     this.save()
-  //     //Alternative:
-  //     //const lisItem = ev.target.closest('.flick')
-  //     //listItem.remove()
-  //   }
-
-  //   //promote
-  //   function myFunction(){
-  //       if (promoteButton.classList.contains('notclicked')){
-  //           this.classList.add('clicked')
-  //           listItem.style.backgroundColor = 'yellow'
-  //           listItem.style.textTransform = "uppercase"
-  //           listItem.style.fontSize = '25px'
-  //           this.classList.remove('notclicked')
-  //       }else{
-  //           listItem.style.backgroundColor = '#DDA0DD'
-  //           this.classList.add('notclicked')
-  //           listItem.style.textTransform = "none"
-  //           listItem.style.fontSize = '18px'
-  //           this.classList.remove('clicked')
-  //       }
-  //   }
-
     //Add flick to this.flicks
     //this.flicks.unshift(flick)
     
     //this.save()
     //unshift method adds element to start of array and returns new length
-    //shift method rmeoves first element from array and returns that element
+    //shift method removes first element from array and returns that element
     
     this.addFlick(flick)
+    
     f.reset()
   },
  
@@ -277,12 +191,12 @@ const app = {
   save(){
     localStorage
       .setItem('flicks', JSON.stringify(this.flicks))
+  
   },
 
   renderListItem(flick) {
     const item = this.template.cloneNode(true)
     item.classList.remove('template')
-    item.dataset.id = flick.id
     item
       .querySelector('.flick-name')
       .textContent = flick.name
